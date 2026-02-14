006ba2f898668030626954883f93378b6d1801d0
andreyk1n <betas23wr@gmail.com>
2026-02-14 18:02:12 +0200

    feat: migrate styles to SCSS and implement footer component

---
+++ src/template-parts/partials/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <div className="footer__container">
+                <p>© 2023</p>
+                <div className="footer__nav">
+                    <a href="#" className="footer__link">Twitter</a>
+                    <a href="#" className="footer__link">LinkedIn</a>
+                    <a href="#" className="footer__link">Email</a>
+                    <a href="#" className="footer__link">RSS feed</a>
+                    <a href="#" className="footer__link">Add to Feedly</a>
+                </div>
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
